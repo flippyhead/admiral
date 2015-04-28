6ecca69ae832cbdd5ad10317b0684bfe72096914
Peter T. Brown <peter@flippyhead.com>
2015-04-28 15:29:18 -0700

    beautiful

--- lib/admiral/cli.rb
+++ lib/admiral/cli.rb
@@ -2,11 +2,5 @@ require 'thor'
 
 module Admiral
   class Cli < Thor
-
-    include Admiral::Tasks::CloudFormation
-    # include Admiral::Tasks::OpsWorks
-    # include Admiral::Tasks::Deploy::Base
-    # include Admiral::Tasks::Deploy::Meteor
-
   end
 end
